c921bd53cb53ca4f99fcd4c29096a26bf84babcf
sgpatel <shiv.cs@hotmail.com>
2021-03-31 11:46:58 +0530

    web-form

---
+++ web-form/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <signup-form/>
+</div>
+</template>
+
+<script>
+import SignupForm from "./components/SignupForm.vue"
+export default {
+  name: 'App',
+  components: {
+    SignupForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
+  align-content: center;
+}
+</style>
