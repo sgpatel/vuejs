7475a3dfd32db8aa242be2915d7affb7e8d6b7e9
sgpatel <shiv.cs@hotmail.com>
2021-04-02 16:47:05 +0530

    add actions

--- project-planner/src/App.vue
+++ project-planner/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-  </div>
+  <Navbar />
   <router-view/>
 </template>
-
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+ components:{Navbar}
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
 }
 
 #nav {
--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="home">
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project='project' @delete="handleDelete" @complete="handleComplete"/>
+    </div>
+  </div>
+  <div v-else>Loading Projects ....</div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
-  name: 'Home',
-  components: {
+  name:'Home',
+  data(){
+    return{
+    projects:[]
+    }
+  },
+  components: {SingleProject},
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then((res)=>res.json())
+    .then((data)=>this.projects=data)
+    .catch(err=>console.log(err.message))
+  },
+  methods:{
+    handleDelete(id){
+      this.projects = this.projects.filter(project=> project.id !== id)
+    },
+    handleComplete(id){
+      let p = this.projects.find(project=>project.id ==id)
+      p.complete = !p.complete
+    }
+    
   }
 }
 </script>
