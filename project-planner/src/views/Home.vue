6ac0ad6e195dd5ac5c497281372ea16a8a6de846
sgpatel <shiv.cs@hotmail.com>
2021-04-02 18:30:37 +0530

    Filtered project

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current=$event" :current="current"/>
   <div v-if="projects.length">
-    <div v-for="project in projects" :key="project.id">
+    <div v-for="project in filteredProject" :key="project.id">
       <SingleProject :project='project' @delete="handleDelete" @complete="handleComplete"/>
     </div>
   </div>
@@ -11,15 +12,17 @@
 
 <script>
 import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name:'Home',
   data(){
     return{
-    projects:[]
+    projects:[],
+    current: 'all'
     }
   },
-  components: {SingleProject},
+  components: {SingleProject,FilterNav},
   mounted(){
     fetch('http://localhost:3000/projects')
     .then((res)=>res.json())
@@ -35,6 +38,17 @@ export default {
       p.complete = !p.complete
     },
     
+  },
+  computed:{
+    filteredProject(){
+      if(this.current==='completed'){
+       return  this.projects.filter((project)=> project.complete)
+      }
+      if(this.current==='ongoing'){
+        return this.projects.filter((project)=> !project.complete)
+      }
+      return this.projects
+    }
   }
 }
 </script>
