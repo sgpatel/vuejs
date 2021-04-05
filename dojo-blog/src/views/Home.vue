f298aea992652c471fcc4c1984f8487ee0d9a5b2
sgpatel <shiv.cs@hotmail.com>
2021-04-05 08:56:19 +0530

    dojo composition API setup example

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+ setup(){
+   console.log('setup')
+ },
+ created(){
+   console.log('created')
+ },
+ mounted(){
+   console.log('mounted')
+ }
+}
+</script>
